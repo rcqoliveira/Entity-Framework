a017d919c3748eae2d9e311f93a38aacea2bb9b9
rcqol <rcqol@DESKTOP-RMT1CKD>
2021-09-14 09:46:34 -0300

    Add script for create tables

---
+++ RC.EntityFramework.Api/Create_Tables.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Product](
+	[Id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[Price] [numeric](12, 2) NOT NULL,
+	[ProductName] [varchar](60) NOT NULL,
+	[Description] [varchar](200) NOT NULL
+)
